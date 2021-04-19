2fdbfb5769af96e5b5ebcc05e34a82b7a829f88e
yosuke-oka <y.oka711@gmail.com>
2021-04-19 15:11:18 +0900

    formula -> expression

--- semantics/src/main.rs
+++ semantics/src/main.rs
@@ -1,28 +1,28 @@
 use std::fmt;
 
-enum Formula {
+enum Expression {
     Number {
         value: u32,
     },
     Add {
-        left: Box<Formula>,
-        right: Box<Formula>,
+        left: Box<Expression>,
+        right: Box<Expression>,
     },
     Multiply {
-        left: Box<Formula>,
-        right: Box<Formula>,
+        left: Box<Expression>,
+        right: Box<Expression>,
     },
 }
 
-impl fmt::Display for Formula {
+impl fmt::Display for Expression {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match *self {
-            Formula::Number { ref value } => write!(f, "{}", value),
-            Formula::Add {
+            Expression::Number { ref value } => write!(f, "{}", value),
+            Expression::Add {
                 ref left,
                 ref right,
             } => write!(f, "{} + {}", left, right),
-            Formula::Multiply {
+            Expression::Multiply {
                 ref left,
                 ref right,
             } => write!(f, "{} * {}", left, right),
@@ -31,15 +31,15 @@ impl fmt::Display for Formula {
 }
 
 fn main() {
-    let formula = Formula::Add {
-        left: Box::new(Formula::Multiply {
-            left: Box::new(Formula::Number { value: 1 }),
-            right: Box::new(Formula::Number { value: 2 }),
+    let expression = Expression::Add {
+        left: Box::new(Expression::Multiply {
+            left: Box::new(Expression::Number { value: 1 }),
+            right: Box::new(Expression::Number { value: 2 }),
         }),
-        right: Box::new(Formula::Multiply {
-            left: Box::new(Formula::Number { value: 3 }),
-            right: Box::new(Formula::Number { value: 4 }),
+        right: Box::new(Expression::Multiply {
+            left: Box::new(Expression::Number { value: 3 }),
+            right: Box::new(Expression::Number { value: 4 }),
         }),
     };
-    println!("{}", formula);
+    println!("{}", expression);
 }
