d952fc48f3b7e7ae90ed25f7394d72efc93244de
yosuke-oka <y.oka711@gmail.com>
2021-08-23 16:51:44 +0900

    add to_dfa_design test

--- automaton/src/nfa.rs
+++ automaton/src/nfa.rs
@@ -295,4 +295,33 @@ mod tests {
             vec![1, 2].iter().cloned().collect()
         );
     }
+
+    #[test]
+    fn nfa_simulation_to_dfa_design_test() {
+        let rulebook = NFARulebook::build(vec![
+            (1, 'a', 1),
+            (1, 'a', 2),
+            (1, FREE_MOVE, 2),
+            (2, 'b', 3),
+            (3, 'b', 1),
+            (3, FREE_MOVE, 2),
+        ]);
+        let nfa_design = NFADesign::new((1, vec![3].iter().cloned().collect(), rulebook));
+        let start_state = nfa_design
+            .to_nfa()
+            .get_current_states()
+            .iter()
+            .cloned()
+            .collect::<BTreeSet<_>>();
+        let sim = NFASimulation::new(nfa_design);
+        let mut state = BTreeSet::new();
+        state.insert(start_state);
+        let dfa_design = sim.to_dfa_design();
+        assert!(!dfa_design.is_accept("aaa"));
+        assert!(dfa_design.is_accept("aab"));
+        assert!(dfa_design.is_accept("bbbabb"));
+        println!("{}", dfa_design.is_accept("aaa"));
+        println!("{}", dfa_design.is_accept("aab"));
+        println!("{}", dfa_design.is_accept("bbbabb"));
+    }
 }
